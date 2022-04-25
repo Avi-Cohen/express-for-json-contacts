86830e659aab25e382c30d4284c93efff51cc646
Avi Cohen <avicohen@Avis-MacBook-Air.local>
2022-04-25 13:59:36 +0300

    serching with debouncing term in query

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,9 +3,10 @@ const basicAuth = require("../util/basic-auth");
 module.exports = (app) => {
   app.get("/api/contacts", (req, res) => {
     try {
-      console.log(req.headers);
-      basicAuth(req.headers.authorization);
-      const contacts = JSON.parse(fs.readFileSync("./contacts0.json"));
+      // basicAuth(req.headers.authorization);
+      const contacts = JSON.parse(fs.readFileSync("./contacts0.json")).filter(
+        (item) => item.email?.toLowerCase().includes(req.query.srsearch)
+      );
       res.send(contacts);
     } catch (e) {
       console.log(e.message);
